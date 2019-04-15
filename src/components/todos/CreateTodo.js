13c0b190e51f525c975b32d5c2bb5e282a8f9c22
Rudy Ravelin <rudy@noukod.com>
2019-04-15 13:33:22 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
- 
+
 class CreateTodo extends Component {
     state = {
         text: ''
     };
- 
+
     handleChange = event => {
         this.setState({
             text: event.target.value
         });
     };
- 
+
     handleSubmit = event => {
         event.preventDefault();
-        this.props.addTodo(this.state);
+        this.props.dispatch({ type: 'ADD_TODO', payload: this.state });
     };
- 
+
     render() {
         return (
             <div>
@@ -35,14 +35,5 @@ class CreateTodo extends Component {
         );
     }
 }
- 
-const mapDispatchToProps = dispatch => {
-    return {
-        addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
-    };
-};
- 
-export default connect(
-    null,
-    mapDispatchToProps
-)(CreateTodo);
+
+export default connect()(CreateTodo);
